fbfe6901c3b9413fe641d359fdc8fdfedffc7d6b
Matt Tanthai Cosh <105581363+TheUnknown550@users.noreply.github.com>
2025-01-29 22:51:48 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS with React and TypeScript!</h1>
+    </div>
+  );
+}
+
+export default App;
